4b16a1dd295a87852ac393402265ac619b597625
Christina <cwiech@alumni.nd.edu>
2016-04-03 15:09:37 -0400

    Changed meta tag info in contact.php file

--- contact/contact.php
+++ contact/contact.php
@@ -18,8 +18,7 @@
 ?><!DOCTYPE html>
 <html lang="en">
 	<head>
-		<meta charset="utf-8" />
-	    <meta http-equiv="x-ua-compatible" content="ie=edge">
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 	    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 		<title>Christina Wiech - Front End Developer</title>
 		<meta name="description" content="Christina Wiech's front end developer portfolio site">
